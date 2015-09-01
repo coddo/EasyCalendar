550b65f2b21e42f974e7baa75f26aeef909938a7
coddo <codobanclaudiu@gmail.com>
2015-09-01 15:52:29 +0300

    Added EntityFramework for SQLite and and started a structure for the DAL package

---
+++ EasyCalendar/DAL/Models/Event.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyCalendar.DAL.Models
+{
+    [Table("event")]
+    public class Event
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+    }
+}
