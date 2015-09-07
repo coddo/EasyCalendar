0e66cc6afc3d0a7e5ac206635a74e2c84226725a
coddo <codobanclaudiu@gmail.com>
2015-09-08 00:10:15 +0300

    Added timer for monitoring whether the app has transitioned to a new day and updates the events interface if necessary. Also sounds alarms

--- EasyCalendar/App/CalendarForm.cs
+++ EasyCalendar/App/CalendarForm.cs
@@ -1,23 +1,55 @@
 ﻿using EasyCalendar.DAL;
 using EasyCalendar.Notifications;
+using System;
 using System.Windows.Forms;
 
 namespace EasyCalendar.App
 {
     public partial class CalendarForm : Form
     {
+        private DateTime previousDate;
+
         public CalendarForm()
         {
             InitializeComponent();
+
+            previousDate = DateTime.Today;
         }
 
-        private void CalendarForm_Load(object sender, System.EventArgs e)
+        private void RunAlarmIfNecessary()
         {
             using (var db = new UnitOfWork())
             {
                 if (db.EventsRepository.HasUpcomingEvents())
                     AlarmCenter.PlayAlarm();
             }
+        }
+
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            // Run the alarm again and refresh the view only if we have passed to a new day
+            if (previousDate != DateTime.Today)
+            {
+                calendarControl.UpdateUI();
+
+                RunAlarmIfNecessary();
+
+                previousDate = DateTime.Today;
+            }
+        }
+
+        private void CalendarForm_Load(object sender, System.EventArgs e)
+        {
+            // Start an alarm if necessary
+            RunAlarmIfNecessary();
+
+            var timer = new Timer
+            {
+                Interval = 1000 * 60 * 60,
+            };
+
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
         private void CalendarForm_Shown(object sender, System.EventArgs e)
